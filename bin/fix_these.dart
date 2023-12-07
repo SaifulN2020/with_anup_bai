5d4f3f1066da9bf404ae9ba828036fbc95cee180
Anup Barua <anupbarua30@gmail.com>
2023-12-08 00:30:03 +0600

    Create fix_these.dart

---
+++ bin/fix_these.dart
@@ -0,0 +1,8 @@
+void main(){
+  /*
+    studentID, grade, teacherID String type hisabe nite bolse support class a.
+    courseScore & couresTought(banan bhul) ai duita main function theke set korte hobe, 
+    class a default hisabe deya jabe na, karon different object ar different List value thakbe.
+    Student class a avarage score ber korar janno akta  function likte hobe.
+  */
+}
